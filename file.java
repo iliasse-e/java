1b9187f2f5045bd3cacb8557d32654f1a7daf8ec
Iliasse <elabouyi.iliasse@gmail.com>
2025-03-19 12:58:10 +0100

    first commit

---
+++ file.java
@@ -0,0 +1,48 @@
+public class file {
+    void main() {
+
+        // Entier
+
+        // 8 bits : de -128 à 127
+        byte a = 127;
+
+        // 16 bits : -32768 à 32767
+        short b = 3333;
+
+        // 32 bits : -2147483648 à 2147483647
+        int c = 2222222;
+
+        // 64 bits : -9223372036854775808 à 9223372036854775807
+        long d = 33L;
+
+
+        // Nombre à virgule
+
+        // 32 bits : 1.401e-045 à 3.40282e+038
+        float e = 1.3f;
+
+        // 64 bits : 2.22507e-308 à 1.79769e+308
+        double f = 2.3;
+
+
+        // Caractères
+
+        char g = 'g';
+
+
+        // Chaines de caractères
+
+        String h = g + "chaine";
+
+
+        // Booléen
+
+        boolean i = false;
+
+
+        // Inférence
+
+        var avecInference = "Quand même une chaîne de caractères";
+
+    }
+}
